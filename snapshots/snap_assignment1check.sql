800c4893a4fc10486ab1ebf6ee0854313b6d7d1a
Jayavani Devarasetty <jayavani.devarasetty@thryvedigital.com>
2025-11-04 04:47:38 +0000

    test

--- snapshots/snap_assignment1check.sql
+++ snapshots/snap_assignment1check.sql
@@ -5,7 +5,6 @@
             target_schema="raw_thryve",
             unique_key="CLAIM_ID",
             strategy="check",
-            ---updated_at="last_update_ts",
             check_cols=["member_id", "enroll_date", "status", "cancel_date"],
             invalidate_hard_deletes=True,
         )
