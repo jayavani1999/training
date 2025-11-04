59f4d374cf3ee13284ecd8ef21d97b53fd2d007e
Jayavani Devarasetty <jayavani.devarasetty@thryvedigital.com>
2025-11-04 04:38:59 +0000

    g

---
+++ snapshots/snap_assignment1check.sql
@@ -0,0 +1,15 @@
+{% snapshot snap_assignment1 %}
+    {{
+        config(
+            target_database="raw",
+            target_schema="raw_thryve",
+            unique_key="CLAIM_ID",
+            strategy="check",
+            ---updated_at="last_update_ts",
+            check_cols=['member_id','enroll_date','status','cancel_date'],
+            invalidate_hard_deletes=True
+                    )
+    }}
+    select *
+    from {{ ref("claim_member") }}
+{% endsnapshot %}
