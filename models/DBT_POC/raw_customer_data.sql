f779771f6502457bf3a5009e5262a013bb0eede9
Jayavani Devarasetty <jayavani.devarasetty@thryvedigital.com>
2025-12-13 16:18:47 +0000

    test

--- models/DBT_POC/raw_customer_data.sql
+++ models/DBT_POC/raw_customer_data.sql
@@ -3,7 +3,7 @@ SELECT
         DATE_TRUNC('month', ORDER_DATE) AS LTV_MONTH, -- Truncate to the beginning of the month
         SUM(TOTAL_AMOUNT) AS total_lifetime_value_month,
         COUNT(DISTINCT ORDER_ID) AS total_orders_month
-    FROM {{ source('dbt_poc', 'RAW_ORDER_DATA') }}
+    FROM {{ source('dbt_poc', 'raw_order_data') }}
     GROUP BY
         CUSTOMER_ID,
         LTV_MONTH
